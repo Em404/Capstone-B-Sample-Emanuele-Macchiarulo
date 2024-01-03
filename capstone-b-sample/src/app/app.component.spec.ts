d1541def2cdf39465e07ae656b8a23be0cffcea1
Em404 <emanuele.macchiarulo.1998@gmail.com>
2024-01-03 12:53:02 +0100

    nuovo progetto con routing

--- capstone-b-sample/src/app/app.component.spec.ts
+++ capstone-b-sample/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'capstone-b-sample' title`, () => {
+  it(`should have as title 'capstone-b-sample'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('capstone-b-sample');
